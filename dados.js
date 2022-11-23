58f93586de234e78635b0873d053d0bf3a36b52e
marianavandrade <114673326+marianavandrade@users.noreply.github.com>
2022-11-23 17:30:25 -0300

    Delete dados.js

--- dados.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('nome').innerHTML = localStorage.getItem('nome');
-document.getElementById('login').innerHTML = localStorage.getItem('login');
-document.getElementById('email').innerHTML = localStorage.getItem('email');
-document.getElementById('senha').innerHTML = localStorage.getItem('senha');
